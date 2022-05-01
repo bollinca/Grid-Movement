508869321b06a5a11e63d2a08a9279b82ea70ea2
bollinca <51376777+bollinca@users.noreply.github.com>
2022-05-01 19:37:49 -0400

    Update PlayerMovement.cs
    - Returns player to previous position if an obstacle is encountered

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,25 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private GameObject player;
-    public Vector3 newPlayerPosition;
-    
+    public Vector3 newPosition;
+    public Vector3 oldPosition;
+
     private float speed;
     private float inputPolarity;
-    
+
     private bool movementCompleted;
     private bool isPlayerMoving;
+
+    private float moveStartTime;
+    private float moveCurrentTime;
+    private float timeOutLimit;
 
     void Start()
     {
         player = gameObject;
         speed = 0.3f;
         isPlayerMoving = false;
+        timeOutLimit = 0.32f;
     }
 
     void FixedUpdate()
@@ -44,29 +50,31 @@ public class PlayerMovement : MonoBehaviour
 
     void SetMoveTarget()
     {
+        //Save previous position in case of obstacles
+        oldPosition = transform.position;
         //determine direction player is moving in and set new destination
         if (Input.GetAxis("Vertical") != 0)
         {
             inputPolarity = Mathf.Sign(Input.GetAxis("Vertical"));
-            newPlayerPosition = new Vector3(transform.position.x, transform.position.y + inputPolarity, transform.position.z);
+            newPosition = new Vector3(transform.position.x, transform.position.y + inputPolarity, transform.position.z);
         }
         else if (Input.GetAxis("Horizontal") != 0)
         {
             inputPolarity = Mathf.Sign(Input.GetAxis("Horizontal"));
-            newPlayerPosition = new Vector3(transform.position.x + inputPolarity, transform.position.y, transform.position.z);
+            newPosition = new Vector3(transform.position.x + inputPolarity, transform.position.y, transform.position.z);
         }
     }
 
     // Moves player gradually
     void MoveTowardsTarget()
     {
-        transform.position = Vector3.MoveTowards(transform.position, newPlayerPosition, Time.fixedDeltaTime * speed);
+        transform.position = Vector3.MoveTowards(transform.position, newPosition, Time.fixedDeltaTime * speed);
     }
 
     //Checks if movement is complete and repeats movement until destination is reached.
     bool IsMovementDone()
     {
-        if (player.transform.position == newPlayerPosition)
+        if (player.transform.position == newPosition)
         {
             movementCompleted = true;
             return movementCompleted;
@@ -74,7 +82,30 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             MoveTowardsTarget();
+            if (IsPlayerStuck())
+            {
+                CancelMovement();
+            }
             return movementCompleted = false;
+        }
+    }
+
+    void CancelMovement()
+    {
+        newPosition = oldPosition;
+    }
+
+    bool IsPlayerStuck()
+    {
+        moveCurrentTime = Time.time;
+        float timeSpentMoving = moveCurrentTime - moveStartTime;
+        if (timeSpentMoving > timeOutLimit)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
@@ -84,6 +115,7 @@ public class PlayerMovement : MonoBehaviour
     {
         isPlayerMoving = true;
         SetMoveTarget();
+        moveStartTime = Time.time;
         yield return new WaitUntil(IsMovementDone);
         isPlayerMoving = false;
     }
